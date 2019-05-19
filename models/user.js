a8bba9937c3256e13706eca3d14b8eabe738c6fe
Mosbycr <mosbycr@gmail.com>
2019-05-19 15:02:37 -0400

    created user table with userName, password, and hint, associated it with spouses table

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    userName: {
+    type: DataTypes.STRING, 
+    allowNull: false
+    }, 
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    hint: DataTypes.STRING
+  });
+
+  User.associate = function(models){
+    User.hasMany(models.Spouse, {});
+  };
+
+  return User;
+};
