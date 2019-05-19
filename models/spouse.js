fde52fbdc0c6c58bface958e37e8d14335c5a19f
Mosbycr <mosbycr@gmail.com>
2019-05-19 15:03:43 -0400

    created spouse table with spouseName, associated with user and has userId foreign key

---
+++ models/spouse.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+  var Spouse = sequelize.define("Spouse", {
+    spouseName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  Spouse.associate = function(models){
+      Spouse.belongsTo(models.User, {
+          foreignKey: {
+              allowNull: false,
+          }
+      });
+  };
+
+  return Spouse;
+};
