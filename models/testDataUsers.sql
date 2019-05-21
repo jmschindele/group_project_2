460315a99e1dbc22611a77b4e9833066359b81d3
rhalecooper <rhalecooper@gmail.com>
2019-05-20 21:39:41 -0400

    add MySQL script testData.sql

--- models/testDataUsers.sql
+++
@@ -1,7 +0,0 @@
-use duet_testdb;
-
-INSERT INTO duet_testdb.users (userName,  `password`, hint, createdAt, updatedAt)
-VALUES ( "robin3", "three", "How mnay blind mice?", now(), now() );
-
-
-SELECT * FROM duet_testdb.users;
