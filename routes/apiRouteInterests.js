b7b58229307134031875a79cc3f5be868a496ea9
rhalecooper <rhalecooper@gmail.com>
2019-05-23 22:34:16 -0400

    aded interests route

---
+++ routes/apiRouteInterests.js
@@ -0,0 +1,23 @@
+var db = require("../models");
+
+module.exports = function (app) {
+
+
+  app.get("/api/interest/:spouseid", function (req, res) {
+    db.Interests.findAll({
+      where: {
+        SpouseId: req.params.spouseid
+      },
+      order: [
+        ['type', 'ASC'],
+        ['note', 'ASC'],
+      ],
+      attributes: ['id', 'type', 'note']
+    }).then(function (Results) {
+      console.log("Results is", Results)
+      res.json(Results);
+    });
+
+  });
+
+};
