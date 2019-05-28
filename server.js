304ea0bf9000afcccd6d10aba9da341962e4193c
Mosbycr <mosbycr@gmail.com>
2019-05-28 19:33:26 -0400

    changed force to true so we can update heroku database - will need to change back to false!

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ require("./routes/htmlRoutes")(app);
 
 
 // CHANGE FORCE BACK TO FALSE!!!!!!!!!!!!!!!!!!!!!!!
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
