6e54326e21c503ba3d50df2a873b98453b81a35a
Mosbycr <mosbycr@gmail.com>
2019-05-29 20:02:06 -0400

    added jsLoveLanguage.js code with if/else if to check when user is logged in. When logged in user's 1st spouse's love table data will be collected.

--- public/js/jsLoveLanguage.js
+++ public/js/jsLoveLanguage.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+  var loggedInUser = localStorage.getItem("loggedIn");
+  console.log(loggedInUser);
 
+  if (loggedInUser === "false") {
+    console.log("Not logged in yet cannot grab spouseId and table data");
+  } else if (loggedInUser === "true") {
+    var getCurrentSpouse = localStorage.getItem("spouseId");
+    console.log("This is the current spouseId: ", getCurrentSpouse);
 
-
-    
+    $.get("/api/lovelang/" + getCurrentSpouse, function(lovedata) {
+      console.log("This is the love table data: ", lovedata);
+    });
+  }
 });
