380ec2dfd8330b2301846fba838b47d46b6fd89b
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-05-30 21:35:02 -0400

    add dates to page on click handler

--- public/js/appInterests.js
+++ public/js/appInterests.js
@@ -1,27 +1,36 @@
-// var spouseId = 1;
+$(document).ready(function() {
+  var loggedInUser = localStorage.getItem("loggedIn");
+  console.log(loggedInUser);
 
-$('#click').on('click', function(){
-   $.ajax("api/interest/" + spouseId, {
-    type: "GET"
-  }).then(function(response) {
-    var isFalse = [];
-    var isTrue = [];
-    console.log("hi", response.length);
-  
-    for (var i = 0; i < response.length; i++) {
+  if (loggedInUser === "false") {
+    console.log("Not logged in yet cannot grab spouseId and table data");
+  } else if (loggedInUser === "true") {
+    var getCurrentSpouse = localStorage.getItem("spouseId");
+    console.log("This is the current spouseId: ", getCurrentSpouse);
 
-   if (response[i].type === false) {
-     isFalse.push(response[i].note);
-   } else {
-     isTrue.push(response[i].note);
-   }
+    $.ajax("api/interest/" + getCurrentSpouse, {
+      type: "GET"
+    }).then(function(response) {
+      var isFalse = [];
+      var isTrue = [];
+      console.log("hi", response.length);
+
+      for (var i = 0; i < response.length; i++) {
+        if (response[i].type === false) {
+          isFalse.push(response[i].note);
+        } else {
+          isTrue.push(response[i].note);
+        }
+      }
+
+      $("#like").empty();
+
+      for (var i = 0; i < isTrue.length; i++) {
+        $("#like").append(`<li>${isTrue[i]}</li>`);
+      }
+      for (var i = 0; i < isFalse.length; i++) {
+        $("#dislike").append(`<li>${isFalse[i]}</li>`);
+      }
+    });
   }
-   
-  for (var i = 0; i < isTrue.length; i++){
-      $('#like').append(`<li>${isTrue[i]}</li>`);
-  }
-  for (var i = 0; i < isFalse.length; i++){
-    $('#dislike').append(`<li>${isFalse[i]}</li>`);
-}
-  });
-  });
+});
